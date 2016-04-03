d8e9a334a848a607e17e1eba1518c8ffb1236451
Ilia Reingold <iliareingold@Ilias-MacBook-Pro.local>
2016-04-03 13:29:06 -0400

    got a map working finally

--- imports/ui/Map.js
+++ imports/ui/Map.js
@@ -1,11 +1,61 @@
 import React from 'react';
+import { Gmaps, Marker, InfoWindow, Circle } from 'react-gmaps';
 
-export default class Map extends React.Component {
+const coords = {
+  lat: 51.5258541,
+  lng: -0.08040660000006028,
+};
+
+export default class App extends React.Component {
+  onMapCreated(map) {
+    map.setOptions({
+      disableDefaultUI: true,
+    });
+  }
+
+  onDragEnd(e) {
+    console.log('onDragEnd', e);
+  }
+
+  onCloseClick() {
+    console.log('onCloseClick');
+  }
+
+  onClick(e) {
+    console.log('onClick', e);
+  }
+
   render() {
     return (
-      <div id="map">
-        I am mapdiv
-      </div>
+      <Gmaps
+        width={'800px'}
+        height={'600px'}
+        lat={coords.lat}
+        lng={coords.lng}
+        zoom={12}
+        loadingMessage={'Be happy'}
+        params={{ v: '3.exp' }}
+        onMapCreated={this.onMapCreated}
+      >
+        <Marker
+          lat={coords.lat}
+          lng={coords.lng}
+          draggable
+          onDragEnd={this.onDragEnd}
+        />
+        <InfoWindow
+          lat={coords.lat}
+          lng={coords.lng}
+          content={'Hello, React :)'}
+          onCloseClick={this.onCloseClick}
+        />
+        <Circle
+          lat={coords.lat}
+          lng={coords.lng}
+          radius={500}
+          onClick={this.onClick}
+        />
+      </Gmaps>
     );
   }
 }
